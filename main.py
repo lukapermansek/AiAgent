3db1ce798a22ecc21de96410b39e442974fbd744
lukapermansek <luka.pike@gmail.com>
2026-03-07 18:42:03 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("API key not found.")
+
+client = genai.Client(api_key=api_key)
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+args = parser.parse_args()
+
+def main():
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents=args.user_prompt)
+
+
+    if response.usage_metadata is None:
+        raise RuntimeError("Likely failed API request.")
+    else:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
