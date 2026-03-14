79c814699999cfc1034471a57943db5bca250b8f
lukapermansek <luka.pike@gmail.com>
2026-03-14 11:00:20 +0100

    added prompts module, where system prompt is determined, also updated the main.py to use the system prompt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import *
 
 def get_client():
     load_dotenv()
@@ -23,8 +24,11 @@ def main():
 
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
-    response = client.models.generate_content(model='gemini-2.5-flash', 
-    contents=messages)
+    response = client.models.generate_content(
+        model='gemini-2.5-flash', 
+        contents=messages,
+        config=types.GenerateContentConfig(system_instruction=system_prompt, temperature=0)
+    )
 
     if response.usage_metadata is None:
         raise RuntimeError("Likely failed API request.")
